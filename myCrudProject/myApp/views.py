30c928d1d391dd400505876efa3af83499321c01
Rahul Kumar Bhagat <rkb.ucet@gmail.com>
2024-03-06 13:37:25 +0530

    POST method created

--- myCrudProject/myApp/views.py
+++ myCrudProject/myApp/views.py
@@ -5,21 +5,38 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 def student_api(request):
     if request.method == 'GET':
-        json_data = request.body
-        stream = io.BytesIO(json_data)
+        jsonData = request.body
+        stream = io.BytesIO(jsonData)
         pythonData = JSONParser().parse(stream)
         id = pythonData.get('id', None)
         if id is not None:
-            student = Student.objects.get(id=id)
-            serializedData  = StudentSerializer(student)
+            studentObjs = Student.objects.get(id=id)
+            serializedData  = StudentSerializer(studentObjs)
             jsonData = JSONRenderer().render(serializedData.data)
             return HttpResponse(jsonData, content_type ='application/json')
         
-    student = Student.objects.all()
-    serializedData = StudentSerializer(student, many=True)
-    jsonData = JSONRenderer().render(serializedData.data)
-    return HttpResponse(jsonData, content_type='application/json')
+        studentObjs = Student.objects.all()
+        serializedData = StudentSerializer(studentObjs, many=True)
+        jsonData = JSONRenderer().render(serializedData.data)
+        return HttpResponse(jsonData, content_type='application/json')
+
+
+    if request.method == 'POST':
+        jsonData = request.body
+        stream = io.BytesIO(jsonData)
+        pythonData = JSONParser().parse(stream)
+        serializedData = StudentSerializer(data = pythonData)
+        if serializedData.is_valid():
+            serializedData.save()
+            response = {'message': 'Data Saved!'}
+            jsonData = JSONRenderer().render(response)
+            return HttpResponse(jsonData, content_type='application/json')
+        jsonData = JSONRenderer().render(serializedData.errors)
+        return HttpResponse(jsonData, content_type='application/json')
+
--- myCrudProject/myClient.py
+++ myCrudProject/myClient.py
@@ -3,14 +3,31 @@ import json
 
 URL = "http://127.0.0.1:8000/studentapi/"
 
-def get_data(id = None):
-    data = {}
-    if id is not None:
-        data = {'id':id}
-    json_data = json.dumps(data)
-    r = requests.get(url= URL, data = json_data)
+#get method
+# def get_data(id = None):
+#     data = {}
+#     if id is not None:
+#         data = {'id':id}
+#     json_data = json.dumps(data)
+#     r = requests.get(url= URL, data = json_data)
+#     data = r.json()
+#     print(data)
+#
+# get_data(1)
+    
+
+
+#post method
+def post_data():
+    data = {
+        'name': 'Rishu Raj',
+        'roll': 104,
+        'city': 'Kolkata'
+    }
+    
+    jsonData = json.dumps(data)
+    r = requests.post(url= URL, data = jsonData)
     data = r.json()
     print(data)
 
-get_data(4)
-
+post_data()
